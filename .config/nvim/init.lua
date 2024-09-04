1c408c62c56987d760881591b6804dc6eaf681c6
AbhinavMishra32 <abhinavmishra3322@gmail.com>
2024-09-04 22:45:55 +0530

    Update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,2 @@
 require("config.lazy")
-reuire("config.options")
+require("config.options")
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,18 @@
+-- Vim general keymaps
+vim.keymap.set('i', 'jk', '<Esc>l', { noremap = true, silent = true })
+
 vim.g.mapleader = " "
 vim.g.maplocal = " "
+
+
+-- Plugin keymaps
+vim.keymap.set("n", "-","<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
