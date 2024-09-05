19488994453b36e07e53064a91791855345c71b8
AbhinavMishra32 <abhinavmishra3322@gmail.com>
2024-09-05 23:10:00 +0530

    update

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -11,7 +11,7 @@ vim.keymap.set("n", "-","<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 
 -- Telescope
-local builtin = require('telescope.builtin')
+-- local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
