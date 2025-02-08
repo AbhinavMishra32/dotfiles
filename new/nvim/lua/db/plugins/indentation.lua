1bb7a67eb61e928a922b801c7c51cd08c253716a
Abhinav Mishra <abhinavmishra3322@gmail.com>
2025-02-08 23:18:25 +0530

    add mini.indentscope and nvim-treesitter plugin configurations

---
+++ new/nvim/lua/db/plugins/indentation.lua
@@ -0,0 +1,55 @@
+return {
+    {
+    "echasnovski/mini.indentscope",
+    version = false, -- wait till new 0.7.0 release to put it back on semver
+    event = "VeryLazy",
+    opts = {
+      -- symbol = "▏",
+      symbol = "│",
+      options = { try_as_border = true },
+        draw = {
+            delay = 100,
+        }
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "Trouble",
+          "alpha",
+          "dashboard",
+          "fzf",
+          "help",
+          "lazy",
+          "mason",
+          "neo-tree",
+          "notify",
+          "snacks_dashboard",
+          "snacks_notif",
+          "snacks_terminal",
+          "snacks_win",
+          "toggleterm",
+          "trouble",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "SnacksDashboardOpened",
+                callback = function(data)
+                    vim.b[data.buf].miniindentscope_disable = true
+                end,
+            })
+        end,
+    },
+        -- disable inent-blankline scope when mini-indentscope is enabled
+        -- {
+        --     "lukas-reineke/indent-blankline.nvim",
+        --     optional = true,
+        --     event = "LazyFile",
+        --     opts = {
+        --         scope = { enabled = false },
+        --     },
+        -- },
+}
