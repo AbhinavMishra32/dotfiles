8f90d446d3b6461ad3825b5a4c53936f2f647643
Abhinav Mishra <abhinavmishra3322@gmail.com>
2025-07-21 09:54:13 +0530

    remove blink cmp

--- new/nvim/lua/db/plugins/lsp/mason.lua
+++ new/nvim/lua/db/plugins/lsp/mason.lua
@@ -41,12 +41,12 @@ return {
 
     mason_tool_installer.setup({
       ensure_installed = {
-        -- "prettier", -- prettier formatter
-        -- "stylua", -- lua formatter
-        -- "isort", -- python formatter
-        -- "black", -- python formatter
-        -- "pylint",
-        -- "eslint_d",
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+        "pylint",
+        "eslint_d",
       },
     })
   end,
