ca7051e67a5b4f1a57ede2c52a898082d1f8bc5d
Abhinav Mishra <abhinavmishra3322@gmail.com>
2025-07-27 21:33:23 +0530

    Update

--- new/nvim/lua/db/plugins/misc.lua
+++ new/nvim/lua/db/plugins/misc.lua
@@ -46,4 +46,19 @@ return {
       keymap.set('n', 'gM', '<CMD>Glance implementations<CR>')
     end
   },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  }
 }
